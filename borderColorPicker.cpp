e98aecd1705dd4c4bcacf49610a1ee7333d9190c
Yoruio <zeleidu@gmail.com>
2020-02-28 22:15:45 -0800

    untest implementation of borderColorPicker stuff

--- borderColorPicker.cpp
+++ borderColorPicker.cpp
@@ -3,9 +3,33 @@
 borderColorPicker::borderColorPicker(unsigned int borderSize, HSLAPixel fillColor, PNG &img, double tolerance)
 {
     /* your code here */
+    this.borderSize = borderSize;
+    this.fillColor = fillColor;
+    this.img = img;
+    this.tolerance = tolerance;
+
 }
 
 HSLAPixel borderColorPicker::operator()(point p)
 {
     /* your code here */
+    int MDS = borderSize*borderSize;
+    HSLAPixel ret;
+    for(int x = -borderSize; x <= borderSize; x++){
+        for(int y = -borderSize; y <= borderSize; y++){
+            if (x*x + y*y <= MDS &&
+            img.getPixel(p.x + x, p.y + y)->dist(fillColor) > tolerance ){
+                ret.h = fillColor.h;
+                ret.s = fillColor.s;
+                ret.l = fillColor.l;
+                ret.a = fillColor.a;
+                return ret;
+            }
+        }
+    }
+    ret.h = img.getPixel(p.x, p.y)->h;
+    ret.s = img.getPixel(p.x, p.y)->s;
+    ret.l = img.getPixel(p.x, p.y)->l;
+    ret.a = img.getPixel(p.x, p.y)->a;
+    return ret;
 }
