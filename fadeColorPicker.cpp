03b4a01b04e9b26016da809bea5609c8cc827d7f
Yoruio <zeleidu@gmail.com>
2020-02-28 21:37:44 -0800

    Euclidean distance implementation

--- fadeColorPicker.cpp
+++ fadeColorPicker.cpp
@@ -9,13 +9,10 @@ HSLAPixel fadeColorPicker::operator()(point p)
 {
     double distanceSquare = (p.x-p.c.x)*(p.x-p.c.x) + (p.y-p.c.y)*(p.y-p.c.y); 
     HSLAPixel ret; 
-    cout<<"fadeFactor: "<<fadeFactor<<"\n"; 
-    cout<<"orig luminance: "<<p.c.color.l<<"\n"; 
-    cout<<"dist^2: "<<distanceSquare<<"\n"; 
     ret.h = p.c.color.h; 
     ret.l = p.c.color.l; 
 
-    for(int i = 0; i < distanceSquare; i++){
+    for(int i = 0; i < sqrt(distanceSquare); i++){
         ret.l *= fadeFactor;  
     }
     ret.s = p.c.color.s; 
