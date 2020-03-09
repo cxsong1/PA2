0c4634bb00d693a3613c7015ca2283910d2e28c7
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-03-08 21:40:01 -0700

    ?

--- filler.cpp
+++ filler.cpp
@@ -108,8 +108,8 @@ template <template <class T> class OrderingStructure> animation filler::fill(Fil
      *        it will be the one we test against.
      *
      */
-    T processing;
-    unordered_set <point> processed;
+    OrderingStructure processing;
+    std::unordered_set <point> processed;
     for(center c : config.centers){
         processing.add(c);
     }
