5ff56cf83464f7cb8edbbcd9bfaa7611b867c8eb
Yoruio <zeleidu@gmail.com>
2020-03-09 23:57:48 -0700

    oosp

--- filler.cpp
+++ filler.cpp
@@ -113,7 +113,7 @@ template <template <class T> class OrderingStructure> animation filler::fill(Fil
 
     //begin initialized to 0
     int processed[config.img.width()][config.img.height()];
-    for(int i = 0; *unsigned long) i < config.img.width(); i++){
+    for(int i = 0; (unsigned long) i < config.img.width(); i++){
         for(int j = 0; (unsigned long) j < config.img.height(); j++){
             processed[i][j] = 0;
         }
